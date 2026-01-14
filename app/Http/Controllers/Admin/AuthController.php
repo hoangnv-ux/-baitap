234381f6f7ead80e8707a4325c3954791e10f8a0
HoangNV <hoangnv@guxplus.com>
2026-01-14 16:32:46 +0700

    change base main

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -30,23 +30,13 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        // Attempt to generate the JWT token
-        if (!$token = auth('admin')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        if(!auth()->guard('admin')->attempt(($credentials))){
+            return redirect()->back()->withErrors(['message' => 'invalid credentials']);
         }
 
-        // Return the generated JWT token
-        return $this->respondWithToken($token, 'admin');
-    }
+        $request->session()->regenerate();
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
-    {
-        return response()->json(auth('admin')->user());
+        return redirect()->route('admin.dashboard');
     }
 
     /**
@@ -58,34 +48,6 @@ class AuthController extends Controller
     {
         auth('admin')->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        $token = JWTAuth::parseToken()->refresh();
-
-        return $this->respondWithToken($token, 'admin');
-    }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token, $guard)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => JWTAuth::factory()->getTTL() * 60  // Convert TTL to seconds
-        ]);
+        return redirect()->route('admin.login');
     }
 }
